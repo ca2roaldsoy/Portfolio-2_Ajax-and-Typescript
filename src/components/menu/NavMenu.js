bfd46cb20d2e0c0949026ddb9813d9ba8939a1f5
Cato <roaldsoyen@gmail.com>
2026-02-02 20:32:42 +0100

    fix nav menu

--- src/components/menu/NavMenu.js
+++ src/components/menu/NavMenu.js
@@ -1,15 +1,14 @@
-import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from "react-router-dom";
-import Home from "../home/Home";
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import {
+  NavLink,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import Character from "../amiiboDetails/Character";
+import Home from "../home/Home";
 
 // Navigation Menu
 function NavMenu() {
@@ -17,7 +16,7 @@ function NavMenu() {
     <Router>
       <Navbar bg="dark" variant="dark" role="navigation" expand="md">
         <Navbar.Brand>
-          <NavLink to="/" exact role="link">
+          <NavLink to="/" exact="true" role="link">
             Amiibo
           </NavLink>
         </Navbar.Brand>
@@ -26,7 +25,7 @@ function NavMenu() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <NavLink to="/" exact className="nav-link" role="link">
+            <NavLink to="/" exact="true" className="nav-link" role="link">
               Home
             </NavLink>
           </Nav>
@@ -35,10 +34,10 @@ function NavMenu() {
 
       {/* Navigating showing correct component*/}
       <Container>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/character/:character" component={Character} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/character/:character" element={<Character />} />
+        </Routes>
       </Container>
     </Router>
   );
